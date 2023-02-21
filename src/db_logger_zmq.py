3efd5c68e7e4599618a383011ef2c73c84089c16
Kirill Bolshakov <k.bolshakov31@yandex.ru>
2023-02-21 23:12:46 +0400

    (new): Реализована работа логера как SUBSCRIBER ZMQ

--- src/db_logger_zmq.py
+++ src/db_logger_zmq.py
@@ -1,5 +1,9 @@
 import zmq
-import Logger
+import db_logger
+
+logger = db_logger.db_logger()
+logger.open_database("../db/test.db")
+# del(db_logger)
 
 context = zmq.Context()
 subscriber = context.socket(zmq.SUB)
@@ -14,9 +18,11 @@ subscriber.setsockopt(zmq.SUBSCRIBE, b"DB_LOGGER")
 try:
 	while True:
 		[address, contents] = subscriber.recv_multipart()
-		print(f"[{address}] {contents}")
+		logger.log_json(contents)
+		# print(f"[{address}] {contents}")
 except KeyboardInterrupt:
 	print("\nStopped by user")
 finally:
 	subscriber.close()
 	context.term()
+	del(logger)
