261974de314f54c578e6d5af01183cb809732374
Kirill Bolshakov <k.bolshakov31@yandex.ru>
2023-02-22 23:13:41 +0400

    (chg): db_logger_zmq реализован как класс.

--- src/db_logger_zmq.py
+++ src/db_logger_zmq.py
@@ -1,28 +1,47 @@
+import time
 import zmq
 import db_logger
 
-logger = db_logger.db_logger()
-logger.open_database("../db/test.db")
-# del(db_logger)
-
-context = zmq.Context()
-subscriber = context.socket(zmq.SUB)
-
-try:
-	subscriber.connect("tcp://localhost:5559")
-except zmq.ZMQError as error:
-	print("Error during connecting to ZMQ proxy:", error)
+class db_logger_zmq(db_logger.db_logger):
 	
-subscriber.setsockopt(zmq.SUBSCRIBE, b"DB_LOGGER")
+	def __init__(self, address):
+		super(db_logger_zmq, self).__init__()
+		self.address = address
 
-try:
-	while True:
-		[address, contents] = subscriber.recv_multipart()
-		logger.log_json(contents)
-		# print(f"[{address}] {contents}")
-except KeyboardInterrupt:
-	print("\nStopped by user")
-finally:
-	subscriber.close()
-	context.term()
-	del(logger)
+	def connect(self):
+		self.context = zmq.Context()
+		self.subscriber = self.context.socket(zmq.SUB)
+        
+		try:
+			self.subscriber.connect(self.address)
+		except zmq.ZMQError as error:
+			print("Error during connecting to ZMQ proxy:", error)
+
+
+	def subscribe(self, l):
+		for i in l:
+			self.subscriber.setsockopt(zmq.SUBSCRIBE, i.encode("utf-8"))	
+
+	def listen(self):
+		try:
+			while True:
+				[address, contents] = self.subscriber.recv_multipart()
+				# logger.log_json(contents)
+				print("Recieved")
+		except KeyboardInterrupt:
+			print("\nStopped by user")
+		finally:
+			self.close()
+
+	def close(self):
+		self.subscriber.close()
+		self.context.term()
+		self.close_database()
+
+
+logger = db_logger_zmq("tcp://localhost:5559")
+logger.open_database("../db/test.db")
+logger.connect()
+logger.subscribe(["MULT", "GEN", ])
+logger.listen()
+del(logger)
